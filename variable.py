67b139918d51f7707143d58c5ec9c6fd322c8a2e
sergeibedrick <sergeibedrick@gmail.com>
2024-09-22 14:57:18 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+course = "Python"
+total_tasks = 12
+hours_spent = 1.5
+average_completion_time = hours_spent / total_tasks
+print('Курс:',course,','' всего задач:',total_tasks,','' затрачено часов:', hours_spent,','' среднее время выполнения:',average_completion_time, 'часа.')
