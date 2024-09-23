79cbade35383cdf6bf2a8d98128d48bd4eed3447
sergeibedrick <sergeibedrick@gmail.com>
2024-09-23 16:02:41 +0300

    Add files via upload

---
+++ dinam_types.py
@@ -0,0 +1,8 @@
+name = "Sergei"
+print("Name:", name, type(name))
+age = 65
+print("Age:", age, type(age))
+new_age = age + 5.5
+print("New age:", new_age, type(new_age))
+is_student = True
+print("Is student:", is_student, type(is_student))
