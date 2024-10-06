41c78948ff796ab7371c1820754223749f90a5d0
sergeibedrick <sergeibedrick@gmail.com>
2024-10-06 15:14:08 +0300

    Add files via upload

--- dop_dz2_hard.py
+++ dop_dz2_hard.py
@@ -2,7 +2,7 @@ def find_valid_pairs(n):
     pairs = []
     for i in range(1, n):
         for j in range(i, n):
-            if n % (i + j) == 0:
+            if n % (i + j) == 0 and (i, j) != (1, 1):
                 pairs.append((i, j))
     return pairs
 
@@ -15,7 +15,7 @@ print()
 # или так
 def find_valid_pairs(n):
     return [(i, j) for i in range(1, n) for j in range(i, n) if n % (i + j)
-            == 0]
+            == 0 and (i, j) != (1, 1)]
 
 results = {n: find_valid_pairs(n) for n in range(3, 21)}
 for n, pairs in results.items():
