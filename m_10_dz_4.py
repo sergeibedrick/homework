5528bd0dda6e5a6a32dfbeb21eb538fe7b426fe3
sergeibedrick <sergeibedrick@gmail.com>
2024-12-13 22:42:00 +0300

    Add files via upload

---
+++ m_10_dz_4.py
@@ -0,0 +1,73 @@
+import threading
+import random
+import time
+from queue import Queue
+
+class Table:
+    """Представляет стол в кафе."""
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+class Guest(threading.Thread):
+    """Представляет гостя в виде потока."""
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        time.sleep(random.randint(3, 10))
+
+class Cafe:
+    """Представляет кафе со столами и очередью гостей."""
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = list(tables)
+
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            for table in self.tables:
+                if table.guest is None:
+                    table.guest = guest
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    break
+            else:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
+
+    def discuss_guests(self):
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
+            for table in self.tables:
+                if table.guest is not None and not table.guest.is_alive():
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+                    if not self.queue.empty():
+                        next_guest = self.queue.get()
+                        table.guest = next_guest
+                        next_guest.start()
+                        print(f"{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+
+# Имена гостей
+guests_names = [
+    'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+    'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+
+# Приём гостей
+cafe.guest_arrival(*guests)
+
+# Обслуживание гостей
+cafe.discuss_guests()
+
