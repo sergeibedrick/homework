6f5962dac69ce2f42e6716daf0a6264a75232ad4
sergeibedrick <sergeibedrick@gmail.com>
2025-01-16 00:53:28 +0300

    Add files via upload

--- m_13_dz_2.py
+++ m_13_dz_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, Router, types
 from aiogram.filters import CommandStart
 
 # Введите токен телеграмм бота
-TOKEN = "Ваш токен"  # Замените на ваш токен
+TOKEN = "ваш токен"  # Замените на ваш токен
 
 # Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
@@ -16,12 +16,12 @@ dp.include_router(router)
 # Обработчик команды /start
 @router.message(CommandStart())
 async def start_handler(message: types.Message):
-    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+    print("Привет! Я бот помогающий твоему здоровью.")
 
 # Обработчик любого сообщения
 @router.message()
 async def all_messages(message: types.Message):
-    await message.answer("Введите команду /start, чтобы начать общение.")
+    print("Введите команду /start, чтобы начать общение.")
 
 
 # Запуск бота
@@ -31,3 +31,5 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+
+
